2b423e6aafd40284f020d78120ef5c1f7fac7a79
Lucas-bit <lucasbertaudgonzalez@gmail.com>
2020-04-11 16:13:23 -0400

    added package.json to root folder

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+//CSS
+import './css/animate.css'
+import './css/bootstrap.min.css'
+import './css/magnific-popup.css'
+import './css/normalize.css'
+import './css/font-awesome.min.css'
+import './css/owl.carousel.css'
+import './css/owl.theme.default.css'
+import './css/responsive.css'
+import './css/style.css'
+import './css/App.css'
+
+//Components
+import Header from './components/Header/header'
+import Jumbotron from './components/Jumbotron/jumbotron'
+import About from './components/About/about'
+import Resume from './components/Resume/resume'
+import MyServices from './components/MyServices/myServices'
+import MyWorks from './components/MyWorks/myWorks'
+import Contact from'./components/Contact/contact'
+import Footer from './components/Footer/footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Jumbotron/>
+      <About/>
+      <Resume/>
+      <MyServices/>
+      <MyWorks/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
