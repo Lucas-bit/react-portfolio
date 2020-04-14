1fc2b1955f55631445c46b1f10d02a7ca856b1b4
Lucas-bit <lucasbertaudgonzalez@gmail.com>
2020-04-13 22:00:38 -0400

    attempted to deploy to github pages

---
+++ build/precache-manifest.b3d0561942c8eba636e10b6eae228dfc.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ea2a67e2a4e4885fa3a8007190b836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6d8b15a5427a797c9ca",
+    "url": "/static/css/main.3e1502eb.chunk.css"
+  },
+  {
+    "revision": "011e7a9af102936c6507",
+    "url": "/static/js/2.e1be3e2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1be3e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d8b15a5427a797c9ca",
+    "url": "/static/js/main.469d7db8.chunk.js"
+  },
+  {
+    "revision": "89282a425a06e45fd0f3",
+    "url": "/static/js/runtime-main.c4d7dc80.js"
+  },
+  {
+    "revision": "eb79a365b4c58e4cc2baca1f1587af49",
+    "url": "/static/media/Trouvaille-screenshot.eb79a365.png"
+  },
+  {
+    "revision": "649e90d3e300e39812531aa0dcdf6f8d",
+    "url": "/static/media/Weather-App (2).649e90d3.png"
+  },
+  {
+    "revision": "be15f234a04efe1be07a989c9772cce4",
+    "url": "/static/media/Workout-App (2).be15f234.png"
+  },
+  {
+    "revision": "964e8c70baaa91cab19f65037380a413",
+    "url": "/static/media/burgerApp.964e8c70.png"
+  },
+  {
+    "revision": "8bcc60aee55b0c030731782d52828497",
+    "url": "/static/media/calendar-app.8bcc60ae.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "279b4926f0b99caca3c4cbb4df58cb45",
+    "url": "/static/media/lucas-headshot.279b4926.jpg"
+  },
+  {
+    "revision": "d921bc7296ed6471644a077f1779e56d",
+    "url": "/static/media/lucas-volcano.d921bc72.jpg"
+  },
+  {
+    "revision": "44091af6388dff5ee4ac5dd7d4fbc894",
+    "url": "/static/media/noteTaker.44091af6.png"
+  }
+]);
